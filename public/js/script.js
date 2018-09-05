c5402354f0d100e004402cf24253f92cb20768b7
gourav sharma <gourav.sharma@clicklabs.co>
2018-09-05 18:47:39 +0530

    simple_portifillo template

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+sticky = document.getElementById("nav").offsetTop;
+nav = document.getElementById("nav").style.height;
+window.addEventListener('scroll',function(){
+    if(scrollY >= sticky){
+        document.getElementById("nav").classList.add("fixed-top");
+    }
+    else{
+        document.getElementById("nav").classList.remove("fixed-top");
+    }
+});
+
+function scrolldown(value){
+    $('html').animate({
+        scrollTop: value-54
+    }, 2000);
+}
