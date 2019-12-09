5aed380f69a5a163b26b3a3f2b1e10ec229b769c
jatr9001 <Trupti.Jadhav.consultant@nielsen.com>
2019-12-09 17:59:19 +0530

    complete home page

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,18 @@
 sticky = document.getElementById("nav").offsetTop;
 nav = document.getElementById("nav").style.height;
-window.addEventListener('scroll',function(){
-    if(scrollY >= sticky){
-        document.getElementById("nav").classList.add("fixed-top");
-    }
-    else{
-        document.getElementById("nav").classList.remove("fixed-top");
-    }
+window.addEventListener("scroll", function() {
+  if (scrollY >= sticky) {
+    document.getElementById("nav").classList.add("fixed-top");
+  } else {
+    document.getElementById("nav").classList.remove("fixed-top");
+  }
 });
 
-function scrolldown(value){
-    $('html').animate({
-        scrollTop: value-54
-    }, 2000);
+function scrolldown(value) {
+  $("html").animate(
+    {
+      scrollTop: value - 54
+    },
+    2000
+  );
 }
